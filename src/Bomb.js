fa1fa4d5f6bde886a00093e6c318dde2e8da695f
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-08-09 23:50:45 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+    const interval = global.setInterval(() => {
+      if (this.state.secondsLeft === 1) {
+        global.clearInterval(interval);
+        console.log('interval cleared');
+      }
+      this.setState({ secondsLeft: this.state.secondsLeft - 1 });
+    }, 1000);
+
+  }
+  render () {
+    return (
+      this.state.secondsLeft !== 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'
+    )
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,13 @@
-// your ImageSlider code here!
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlideIndex : 0
+    };
+  }
+  render () {
+    return (<div>I am on slide {this.state.currentSlideIndex}</div>);
+  }
+}
